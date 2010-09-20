670b78058677ac5ab034ffcaa4f5b653b5d1f201
Thilo Rörig <roerig@math.tu-berlin.de>
2010-09-20 14:20:11 +0000

    conical functional implemented
    git-svn-id: http://fs:8000/svn/DDG@3175 986badc2-2932-0410-96f0-8e0caa59e138

---
+++ src/de/varylab/varylab/math/functional/ConicalFunctional.java
@@ -0,0 +1,161 @@
+package de.varylab.varylab.math.functional;
+
+import java.util.List;
+
+import de.jreality.math.Rn;
+import de.jtem.halfedge.Edge;
+import de.jtem.halfedge.Face;
+import de.jtem.halfedge.HalfEdgeDataStructure;
+import de.jtem.halfedge.Vertex;
+import de.jtem.halfedge.util.HalfEdgeUtils;
+import de.jtem.halfedgetools.functional.DomainValue;
+import de.jtem.halfedgetools.functional.Energy;
+import de.jtem.halfedgetools.functional.Functional;
+import de.jtem.halfedgetools.functional.FunctionalUtils;
+import de.jtem.halfedgetools.functional.Gradient;
+import de.jtem.halfedgetools.functional.Hessian;
+import de.jtem.halfedgetools.util.HalfEdgeUtilsExtra;
+
+public class ConicalFunctional<
+		V extends Vertex<V, E, F>, 
+		E extends Edge<V, E, F>, 
+		F extends Face<V, E, F>> 
+	implements Functional<V, E, F> {
+
+	@Override
+	public <
+		HDS extends HalfEdgeDataStructure<V, E, F>>
+	void evaluate(HDS hds, DomainValue x, Energy E, Gradient G, Hessian H) {
+		if (E != null) {
+			E.set(evaluate(hds, x));
+		}
+		if (G != null) {
+			evaluateGradient(hds, x, G);
+		}
+	}
+
+	// Calculate the energy of a given configuration
+	public double evaluate(HalfEdgeDataStructure<V, E, F> hds, DomainValue x) {
+		double result = 0.0;
+		double[]
+		       vi = new double[3],
+		       vj = new double[3],
+		       vk = new double[3];
+		for (V v : hds.getVertices()) {
+			if(!HalfEdgeUtils.isBoundaryVertex(v)) {
+				FunctionalUtils.getPosition(v, x, vj);
+				List<V> star = HalfEdgeUtilsExtra.getVertexStar(v);
+				int nn = star.size();
+				if(nn == 4) { // vertex has an even number of neighbors!
+					E e = v.getIncomingEdge();
+					
+					double vertexEnergy = 0.0;
+					FunctionalUtils.getPosition(v, x, vj);
+					double sign = -1.0;
+					do {
+						FunctionalUtils.getPosition(e.getStartVertex(), x, vi);
+						FunctionalUtils.getPosition(e.getNextEdge().getTargetVertex(), x, vk);
+						double angle = FunctionalUtils.angle(vi, vj, vk);
+						vertexEnergy += sign*angle;
+						sign *= -1.0;
+						e = e.getNextEdge().getOppositeEdge();
+					} while(e != v.getIncomingEdge());
+					result += vertexEnergy*vertexEnergy;
+				} else { // non degree 4 vertices
+					
+				}	
+			} else { // boundary vertex.
+				
+			}
+		}
+		return result;
+	}
+
+	public void evaluateGradient(
+		//input
+			HalfEdgeDataStructure<V, E, F> hds,
+			DomainValue x,
+		//output
+			Gradient G
+	) {
+		double[] 
+		       vv = new double[3],
+		       vs = new double[3],
+		       vt = new double[3],
+		       ds = new double[3],
+		       dv = new double[3],
+		       dt = new double[3];
+		G.setZero();
+		for (V v : hds.getVertices()) {
+			FunctionalUtils.getPosition(v,x,vv);
+			if(!HalfEdgeUtils.isBoundaryVertex(v)) {
+				FunctionalUtils.getPosition(v, x, vv);
+				List<V> star = HalfEdgeUtilsExtra.getVertexStar(v);
+				int nn = star.size();
+				if(nn == 4) { // vertex has an even number of neighbors!
+					double vertexEnergy = 0.0;
+					FunctionalUtils.getPosition(v, x, vv);
+					E e = v.getIncomingEdge();
+					double sign = -1.0;
+					do {
+						FunctionalUtils.getPosition(e.getStartVertex(), x, vs);
+						FunctionalUtils.getPosition(e.getNextEdge().getTargetVertex(), x, vt);
+						double angle = FunctionalUtils.angle(vs, vv, vt);
+						vertexEnergy += sign*angle;
+						sign *= -1.0;
+						e = e.getNextEdge().getOppositeEdge();
+					} while(e != v.getIncomingEdge());
+					sign = -1.0;
+					do {
+						int 
+							sind = e.getStartVertex().getIndex(),
+							vind = e.getTargetVertex().getIndex(),
+							tind = e.getNextEdge().getTargetVertex().getIndex();
+						
+						FunctionalUtils.getPosition(e.getStartVertex(), x, vs);
+						FunctionalUtils.getPosition(e.getNextEdge().getTargetVertex(), x, vt);
+						FunctionalUtils.angleGradient(vs,vv,vt,ds,dv,dt);
+						double scale = sign*2.0*vertexEnergy;
+						sign *= -1.0;
+						Rn.times(ds,scale,ds);
+						Rn.times(dv,scale,dv);
+						Rn.times(dt,scale,dt);
+						
+						FunctionalUtils.addVectorToGradient(G, 3*sind, ds);
+						FunctionalUtils.addVectorToGradient(G, 3*vind, dv);
+						FunctionalUtils.addVectorToGradient(G, 3*tind, dt);						e = e.getNextEdge().getOppositeEdge();
+					} while(e != v.getIncomingEdge());
+				} else { // non degree 4 vertices
+					
+				}	
+			} else { // boundary vertex.
+				
+			}
+		}
+	}
+
+	public void evaluateHessian(
+		// input
+			HalfEdgeDataStructure<V, E, F> G, DomainValue x,
+		// output
+			Hessian hess) {
+	}
+
+	@Override
+	public <HDS extends HalfEdgeDataStructure<V, E, F>> int getDimension(HDS hds) {
+		return 3*hds.numVertices();
+	}
+
+	@Override
+	public <HDS extends HalfEdgeDataStructure<V, E, F>> int[][] getNonZeroPattern(
+			HDS hds) {
+		return null;
+	}
+
+	@Override
+	public boolean hasHessian() {
+		return false;
+	}
+
+
+}
---
+++ src/de/varylab/varylab/plugin/meshoptimizer/ConicalOptimizer.java
@@ -0,0 +1,36 @@
+package de.varylab.varylab.plugin.meshoptimizer;
+
+import de.jtem.halfedgetools.functional.Functional;
+import de.jtem.jrworkspace.plugin.PluginInfo;
+import de.varylab.varylab.hds.VEdge;
+import de.varylab.varylab.hds.VFace;
+import de.varylab.varylab.hds.VHDS;
+import de.varylab.varylab.hds.VVertex;
+import de.varylab.varylab.math.functional.ConicalFunctional;
+import de.varylab.varylab.plugin.OptimizerPlugin;
+import de.varylab.varylab.plugin.ui.image.ImageHook;
+
+public class ConicalOptimizer extends OptimizerPlugin {
+
+	private ConicalFunctional<VVertex, VEdge, VFace>
+		functional = new ConicalFunctional<VVertex, VEdge, VFace>();
+	
+	@Override
+	public Functional<VVertex, VEdge, VFace> getFunctional(VHDS hds) {
+		return functional;
+	}
+
+	@Override
+	public String getName() {
+		return "Conical Energy";
+	}
+	
+	@Override
+	public PluginInfo getPluginInfo() {
+		PluginInfo info = new PluginInfo("Conical Optimizer", "Thilo Roerig");
+		info.icon = ImageHook.getIcon("angle.png");
+		return info;
+	}
+
+
+}
