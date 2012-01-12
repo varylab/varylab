fed9cb2d84442473e2f6cbf0a4ee971c1780050f
Christoph Seidel <seidel@math.tu-berlin.de>
2012-01-12 15:54:50 +0000

    git-svn-id: http://fs:8000/svn/DDG@3699 986badc2-2932-0410-96f0-8e0caa59e138

---
+++ src/de/varylab/varylab/plugin/nurbs/math/PointDistanceCalculator.java
@@ -0,0 +1,17 @@
+package de.varylab.varylab.plugin.nurbs.math;
+
+import de.jreality.math.Rn;
+import de.varylab.varylab.plugin.nurbs.NURBSSurface;
+
+public class PointDistanceCalculator {
+	
+	public static double[] surfacePoint(double[] point, NURBSSurface ns){
+		double[] p = {0,0,0};
+		return p;
+	}
+	
+	public static double distance(double[] surfacePoint, double[] point){
+		return Rn.euclideanDistance(surfacePoint, point);
+	}
+
+}
---
+++ src/de/varylab/varylab/plugin/nurbs/math/TaoExample.java
@@ -0,0 +1,58 @@
+package de.varylab.varylab.plugin.nurbs.math;
+
+
+import static de.jtem.jpetsc.InsertMode.INSERT_VALUES;
+import de.jtem.jpetsc.Mat;
+import de.jtem.jpetsc.Vec;
+import de.jtem.jtao.Tao;
+import de.jtem.jtao.Tao.GetSolutionStatusResult;
+import de.jtem.jtao.TaoAppAddCombinedObjectiveAndGrad;
+import de.jtem.jtao.TaoAppAddHess;
+import de.jtem.jtao.TaoApplication;
+
+public class TaoExample {
+	public static void main(String[] args){
+		Tao.Initialize();
+		
+		Tao t = new Tao(Tao.Method.CG);
+		Vec v = new Vec(1);
+		v.setValue(0, 100, INSERT_VALUES);
+		v.assemble();
+		Mat m = Mat.createSeqAIJ(1, 1, 1, null);
+		m.assemble();
+	
+		TaoApplication app = new App1();
+		app.setInitialSolutionVec(v);
+		app.setHessianMat(m, m);
+		t.setTolerances(0, 0, 0, 0);
+		t.setApplication(app);
+		t.solve();
+		
+		GetSolutionStatusResult res = t.getSolutionStatus();
+		
+		System.out.printf("iterations = " + res.iterate + "\n");
+		
+		double x = app.getSolutionVec().getValue(0);
+		System.out.printf("pos = " + x  + "\n");
+		System.out.printf("value = " + res.f + "\n");
+		
+		Tao.Finalize();
+	}
+	
+	public static class App1 
+		extends TaoApplication	
+		implements TaoAppAddCombinedObjectiveAndGrad, TaoAppAddHess 
+	{	
+		public double evaluateObjectiveAndGradient(Vec xv, Vec gv) {
+			double x = xv.getValue(0);
+			gv.setValue(0, 2*x, INSERT_VALUES);
+			gv.assemble();
+			return x*x;
+		}
+		public PreconditionerType evaluateHessian(Vec x, Mat H, Mat Hpre) {
+			H.setValue(0, 0, 2, INSERT_VALUES);
+			H.assemble();
+			return PreconditionerType.SAME_PRECONDITIONER;
+		}
+	}
+}
