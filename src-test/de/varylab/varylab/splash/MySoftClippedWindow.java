d09f0853b249eab87ba34257d03dd0f6292dcfc4
Stefan Sechelmann <sechel@math.tu-berlin.de>
2013-02-26 17:10:39 +0000

    splash tests revisited
    git-svn-id: http://fs:8000/svn/DDG@4202 986badc2-2932-0410-96f0-8e0caa59e138

--- src-test/de/varylab/varylab/splash/MySoftClippedWindow.java
+++ src-test/de/varylab/varylab/splash/MySoftClippedWindow.java
@@ -1,17 +1,11 @@
 package de.varylab.varylab.splash;
-import java.awt.AlphaComposite;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.GraphicsConfiguration;
-import java.awt.GridBagLayout;
 import java.awt.Image;
 import java.awt.RenderingHints;
-import java.awt.Transparency;
-import java.awt.geom.AffineTransform;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -32,21 +26,15 @@ public class MySoftClippedWindow extends JPanel     {
         try {
 			image = ImageIO.read(MySoftClippedWindow.class.getResourceAsStream("varylab_color_01.png"));
 		} catch (IOException e1) {}
-        setLayout(new GridBagLayout());
     }
 
     @Override
     public void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g.create();
-
-        int width = getWidth();
-        int height = getHeight();
-
-        // Create a soft clipped image for the background
-        BufferedImage img = java_2d_tricker(g2d, width, height);
-        g2d.drawImage(img, 0, 0, null);
-
-        g2d.dispose();
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+        g2d.drawImage(image, 0, 0, image.getWidth(this)/ 2, image.getHeight(this) / 2, this);
+        g2d.setColor(Color.BLACK);
     }
 
     public static void main(String[] args) {
@@ -67,30 +55,4 @@ public class MySoftClippedWindow extends JPanel     {
         });
     }
 
-    /*
-     * This code is taken from
-     * http://weblogs.java.net/blog/campbell/archive/2006/07/java_2d_tricker.html
-     */
-    private BufferedImage java_2d_tricker(Graphics2D g2d, int width, int height) {
-    	AffineTransform T = new AffineTransform();
-        T.scale(0.5, 0.5);
-        GraphicsConfiguration gc = g2d.getDeviceConfiguration();
-        BufferedImage img = gc.createCompatibleImage(width, height, Transparency.TRANSLUCENT);
-        Graphics2D g2 = img.createGraphics();
-        g2.setComposite(AlphaComposite.Clear);
-        g2.fillRect(0, 0, width, height);
-        g2.setComposite(AlphaComposite.Src);
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-        
-        g2.setColor(Color.WHITE);
-        g2.drawImage(image, T, this);
-//        g2.fillOval(width / 4, height / 4, width / 2, height / 2);
-        g2.setComposite(AlphaComposite.SrcAtop);
-//        g2.setPaint(new GradientPaint(0, 0, Color.RED, 0, height, Color.YELLOW));
-        g2.fillRect(0, 0, width, height);
-        g2.drawImage(image, T, this);
-        g2.dispose();
-        return img;
-    }
 }
