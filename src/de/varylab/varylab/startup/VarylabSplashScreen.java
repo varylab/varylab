ddf508e5e9353ee7447c5a777bc69ced5e87c244
Stefan Sechelmann <sechel@math.tu-berlin.de>
2013-02-26 18:19:41 +0000

    varylab startup screen uses high and low res images
    git-svn-id: http://fs:8000/svn/DDG@4205 986badc2-2932-0410-96f0-8e0caa59e138

--- src/de/varylab/varylab/startup/VarylabSplashScreen.java
+++ src/de/varylab/varylab/startup/VarylabSplashScreen.java
@@ -1,29 +1,64 @@
 package de.varylab.varylab.startup;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Rectangle;
+import java.awt.Toolkit;
 
-import javax.swing.JLabel;
 import javax.swing.JProgressBar;
 
 import de.jtem.jrworkspace.plugin.simplecontroller.widget.SplashScreen;
 import de.varylab.varylab.plugin.ui.image.ImageHook;
+import de.varylab.varylab.startup.image.SplashImageHook;
 
 public class VarylabSplashScreen extends SplashScreen {
 
 	private static final long 
 		serialVersionUID = 1L;
-	private JLabel
-		image = new JLabel(ImageHook.getIcon("splashJReality01.png"));
+	private Image
+		lowResImage = SplashImageHook.getImage("varylab_01_low_res.png"),
+		hiResImage = SplashImageHook.getImage("varylab_01_high_res.png");
 	private JProgressBar
 		progressBar = new JProgressBar(0, 100);
+	private boolean
+		useHighRes = false;
 	
 	public VarylabSplashScreen() {
+		com.sun.awt.AWTUtilities.setWindowOpaque(this, false);
+		int res = Toolkit.getDefaultToolkit().getScreenResolution();
+		useHighRes = res > 75;
+		System.out.println("Splash resolution: " + res + "dpi");
 		setIconImage(ImageHook.getImage("main_03.png"));
 		setLayout(new BorderLayout());
-		add(image, BorderLayout.CENTER);
-		add(progressBar, BorderLayout.SOUTH);
-		progressBar.setStringPainted(true);
+		Dimension size = new Dimension();
+		if (useHighRes) {
+			size.width = hiResImage.getWidth(this) / 2;
+			size.height = hiResImage.getWidth(this) / 2;
+		} else {
+			size.width = lowResImage.getWidth(this);
+			size.height = lowResImage.getWidth(this);
+		}
+		setSize(size);
+		setPreferredSize(size);
+		setMinimumSize(size);
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		Graphics2D g2d = (Graphics2D)g;
+		if (useHighRes) {
+			int w = hiResImage.getWidth(this);
+			int h = hiResImage.getHeight(this);
+			g2d.drawImage(hiResImage, 0, 0, w/2, h/2, this);
+		} else {
+			int w = lowResImage.getWidth(this);
+			int h = lowResImage.getHeight(this);
+			g2d.drawImage(hiResImage, 0, 0, w, h, this);
+		}
 	}
 	
 	@Override
@@ -40,6 +75,11 @@ public class VarylabSplashScreen extends SplashScreen {
 		progressBar.setValue((int)(progress * 100));
 		Rectangle r = new Rectangle(getWidth(), getHeight());
 		getRootPane().paintImmediately(r);
+	}
+	
+	public static void main(String[] args) {
+		VarylabSplashScreen splash = new VarylabSplashScreen();
+		splash.setVisible(true);
 	}
 
 }
