922b3193397510f15de5ebcc8fcf09f778749bfc
Stefan Sechelmann <sechel@math.tu-berlin.de>
2013-03-15 14:24:32 +0000

    include quad, checker, hex, and triangle pattern textures. remove default jreality textures.
    git-svn-id: http://fs:8000/svn/DDG@4261 986badc2-2932-0410-96f0-8e0caa59e138

--- src/de/varylab/varylab/plugin/VarylabMain.java
+++ src/de/varylab/varylab/plugin/VarylabMain.java
@@ -3,7 +3,10 @@ package de.varylab.varylab.plugin;
 import de.jreality.plugin.basic.InfoOverlayPlugin;
 import de.jreality.plugin.basic.View;
 import de.jreality.plugin.basic.ViewToolBar;
+import de.jreality.plugin.content.ContentAppearance;
 import de.jreality.plugin.menu.BackgroundColor;
+import de.jreality.ui.AppearanceInspector;
+import de.jreality.ui.TextureInspector;
 import de.jtem.halfedgetools.adapter.generic.UndirectedEdgeIndex;
 import de.jtem.halfedgetools.plugin.AlgorithmDropdownToolbar;
 import de.jtem.halfedgetools.plugin.HalfedgeInterface;
@@ -22,6 +25,36 @@ public class VarylabMain extends Plugin {
 	@Override
 	public void install(Controller c) throws Exception {
 		super.install(c);
+		configureHalfedgeInterface(c);
+		adapterViewToUI(c);
+		hideInfoOverlayWorkaround(c);
+		
+		ContentAppearance ca = c.getPlugin(ContentAppearance.class);
+		AppearanceInspector ai = ca.getAppearanceInspector();
+		TextureInspector ti = ai.getTextureInspector();
+		ti.getTextures().remove("2 Metal Grid");
+		ti.getTextures().remove("3 Metal Floor");
+		ti.getTextures().remove("4 Chain-Link Fence");
+		ti.getTextures().put("Quads", "de/varylab/varylab/data/quads01.png");
+		ti.getTextures().put("Checker", "de/varylab/varylab/data/checker03.png");
+		ti.getTextures().put("Hex", "de/varylab/varylab/data/hex_pattern.png");
+		ti.getTextures().put("Tri", "de/varylab/varylab/data/triangle_pattern.png");
+		String selectedTex = ti.getTexture();
+		ti.setTexture("Quads");
+		ti.setTexture(selectedTex);
+	}
+
+
+	public void adapterViewToUI(Controller c) {
+		String color = c.getProperty(BackgroundColor.class, "color", "");
+		if (color.equals("")) {
+			BackgroundColor bgColorPlugin = c.getPlugin(BackgroundColor.class);
+			bgColorPlugin.setColor("UI Background");
+		}
+	}
+
+
+	public void configureHalfedgeInterface(Controller c) {
 		HalfedgeInterface hif = c.getPlugin(HalfedgeInterface.class);
 		hif.addAdapter(new VPositionAdapter(), true);
 		hif.addAdapter(new VTexturePositionAdapter(), true);
@@ -31,11 +64,10 @@ public class VarylabMain extends Plugin {
 		hif.addAdapter(new UndirectedEdgeIndex(), true);
 		hif.setTemplateHDS(new VHDS());
 		hif.set(new VHDS());
-		String color = c.getProperty(BackgroundColor.class, "color", "");
-		if (color.equals("")) {
-			BackgroundColor bgColorPlugin = c.getPlugin(BackgroundColor.class);
-			bgColorPlugin.setColor("UI Background");
-		}
+	}
+
+
+	public void hideInfoOverlayWorkaround(Controller c) {
 		c.getPlugin(View.class);
 		c.getPlugin(DomainVisualisationPlugin.class);
 		InfoOverlayPlugin iol = c.getPlugin(InfoOverlayPlugin.class);
@@ -44,6 +76,11 @@ public class VarylabMain extends Plugin {
 		toolbar.setFloatable(false);
 		AlgorithmDropdownToolbar algoToolbar = c.getPlugin(AlgorithmDropdownToolbar.class);
 		algoToolbar.setFloatable(false);
+	}
+	
+	
+	protected void replaceDefaultTextures(ContentAppearance ca) {
+		
 	}
 	
 	
